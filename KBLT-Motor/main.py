db072f83db0312445caa9ce411437065bc0b7f21
desplenterkarel <57403267+desplenterkarel@users.noreply.github.com>
2024-08-01 15:39:57 +0200

    Update main.py
    to be in line with code on radxa

--- KBLT-Motor/main.py
+++ KBLT-Motor/main.py
@@ -4,11 +4,11 @@ from motor import Motor
 def main():
     pulse_pin = 156
     direction_pin = 154
+    steps = 200
+    pulse_duration = 0.002
+    hold_duration = 0.5
 
-    pulse_duration = 0.001
-    hold_duration = 1
-
-    motor = Motor(pulse_pin, direction_pin)
+    motor = Motor(pulse_pin, direction_pin, steps)
 
     try:
         while True:
